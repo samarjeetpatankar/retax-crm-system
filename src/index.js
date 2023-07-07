6fb2b3a54fcafac3daaf8f4fecbbe41f5033ee64
shubhambhargav10 <shubhambhargav10@gmail.com>
2023-07-07 23:03:46 +0530

    fjef

--- src/index.js
+++ src/index.js
@@ -2,19 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { BrowserRouter } from 'react-router-dom';
-import { ChakraProvider } from '@chakra-ui/react';
-import { Provider } from 'react-redux';
-import store from './Redux/store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render (
-      <BrowserRouter>
-        <ChakraProvider>
-            <Provider store={store}>
+      
               <App />
-            </Provider>
-        </ChakraProvider>
-        </BrowserRouter>      
+                
 );
 
