c827c65bc48cdf82436c1fa7f5ed39a9f3307930
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-07-08 12:28:48 +0530

    adding logos

--- src/Components/AllRoutes/AllRoutes.jsx
+++ src/Components/AllRoutes/AllRoutes.jsx
@@ -1,29 +1,30 @@
-import {Routes, Route} from 'react-router-dom'
-import Dashboard from "../Dashboard/Dashboard"
-import Help from '../Help/Help'
-import Employee from '../Empolyee Page/Empolyee'
-import EmployeeDetails from '../Empolyee Page/EmpolyeeDetails'
-import AddEmpolyee from '../Empolyee Page/AddEmpolyee'
-import Cases from '../Cases/Cases'
-import Settings from '../Settings/Settings'
-import Calendar1 from '../Calender/calander'
-
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "../Dashboard/Dashboard";
+import Help from "../Help/Help";
+import Employee from "../Empolyee Page/Empolyee";
+import EmployeeDetails from "../Empolyee Page/EmpolyeeDetails";
+import AddEmpolyee from "../Empolyee Page/AddEmpolyee";
+import Cases from "../Cases/Cases";
+import Settings from "../Settings/Settings";
+import Calendar1 from "../Calender/calander";
 
 const AllRoutes = () => {
-    return <Routes>
-        <Route path='/' element = {<Dashboard/>}/>
-        <Route path='/calendar' element = {<Calendar1/>}/>
-        <Route path={'/employee'} element={<Employee />} /> 
-        <Route path={'/employee/:emp_id'} element={<EmployeeDetails />} /> 
-        <Route path={'/addempolyee'} element={<AddEmpolyee/>} />
-        <Route path='/customers' element = {<Dashboard/>}/>
-        <Route path='/cases' element = {<Cases/>}/>
-        <Route path='/leads' element = {<Dashboard/>}/>
-        <Route path='/requests' element = {<Dashboard/>}/>
-        <Route path='/settings' element = {<Settings/>}/>
-        <Route path='/help' element = {<Help/>}/>
-        <Route path='/logout' element = {<Dashboard/>}/>
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/calendar" element={<Calendar1 />} />
+      <Route path={"/employee"} element={<Employee />} />
+      <Route path={"/employee/:emp_id"} element={<EmployeeDetails />} />
+      <Route path={"/addempolyee"} element={<AddEmpolyee />} />
+      <Route path="/customers" element={<Dashboard />} />
+      <Route path="/cases" element={<Cases />} />
+      <Route path="/leads" element={<Cases />} />
+      <Route path="/requests" element={<Cases />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="/help" element={<Help />} />
+      <Route path="/logout" element={<Dashboard />} />
     </Routes>
-}
+  );
+};
 
-export {AllRoutes}
+export { AllRoutes };
