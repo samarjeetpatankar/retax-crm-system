f1b0d3765670939a779ea0cbd46ed63dd9bb7c75
shubhambhargav10 <shubhambhargav10@gmail.com>
2023-07-08 11:16:15 +0530

    testing

--- src/Components/AllRoutes/AllRoutes.jsx
+++ src/Components/AllRoutes/AllRoutes.jsx
@@ -6,13 +6,14 @@ import EmployeeDetails from '../Empolyee Page/EmpolyeeDetails'
 import AddEmpolyee from '../Empolyee Page/AddEmpolyee'
 import Cases from '../Cases/Cases'
 import Settings from '../Settings/Settings'
+import Calendar1 from '../Calender/calander'
 
 
 const AllRoutes = () => {
     return <Routes>
         <Route path='/' element = {<Dashboard/>}/>
         <Route path='/tasks' element = {<Dashboard/>}/>
-        <Route path='/calendar' element = {<Dashboard/>}/>
+        <Route path='/calendar' element = {<Calendar1/>}/>
         <Route path={'/employee'} element={<Employee />} /> 
         <Route path={'/employee/:emp_id'} element={<EmployeeDetails />} /> 
         <Route path={'/addempolyee'} element={<AddEmpolyee/>} />
