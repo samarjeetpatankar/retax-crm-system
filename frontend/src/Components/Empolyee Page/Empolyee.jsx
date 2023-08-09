7f2dda3849198af06d62701443c2cfa23c6156a2
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-08-09 20:38:05 +0530

    making files

--- frontend/src/Components/Empolyee Page/Empolyee.jsx
+++ frontend/src/Components/Empolyee Page/Empolyee.jsx
@@ -248,3 +248,4 @@ function Employee() {
 }
 
 export default Employee;
+ 
--- frontend/src/Components/Sidebar/Sidebar.jsx
+++ frontend/src/Components/Sidebar/Sidebar.jsx
@@ -1,27 +1,34 @@
-import { Box, Heading, Input,Divider, HStack, VStack } from "@chakra-ui/react";
-import {  FaCalendar, FaUserFriends, FaUser, FaBriefcase, FaChartBar, FaCommentAlt, FaCog, FaQuestionCircle, FaSignOutAlt } from "react-icons/fa";
-import { RxDashboard } from "react-icons/rx"
-import { Link } from 'react-router-dom';
-import { useState } from 'react';
+import { Box, Heading, Input, Divider, HStack, VStack } from "@chakra-ui/react";
+import {
+  FaCalendar,
+  FaUserFriends,
+  FaUser,
+  FaBriefcase,
+  FaChartBar,
+  FaCommentAlt,
+  FaCog,
+  FaQuestionCircle,
+  FaSignOutAlt,
+} from "react-icons/fa";
+import { RxDashboard } from "react-icons/rx";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Sidebar = () => {
- 
-  const [searchText, setSearchText] = useState('');
+  const [searchText, setSearchText] = useState("");
 
   const handleSearchChange = (event) => {
     setSearchText(event.target.value);
   };
 
-  
-
   const filteredLinks = [
-    { path: '/', label: 'Dashboard', icon: RxDashboard },
-    { path: '/calendar', label: 'Calendar', icon: FaCalendar },
-    { path: '/employee', label: 'Employees', icon: FaUserFriends },
-    { path: '/customers', label: 'Customers', icon: FaUser },
-    { path: '/cases', label: 'Cases', icon: FaBriefcase },
-    { path: '/leads', label: 'Leads', icon: FaChartBar },
-    { path: '/requests', label: 'Requests', icon: FaCommentAlt },
+    { path: "/", label: "Dashboard", icon: RxDashboard },
+    { path: "/calendar", label: "Calendar", icon: FaCalendar },
+    { path: "/employee", label: "Employees", icon: FaUserFriends },
+    { path: "/customers", label: "Customers", icon: FaUser },
+    { path: "/cases", label: "Cases", icon: FaBriefcase },
+    { path: "/leads", label: "Leads", icon: FaChartBar },
+    { path: "/requests", label: "Requests", icon: FaCommentAlt },
   ].filter((link) =>
     link.label.toLowerCase().includes(searchText.toLowerCase())
   );
@@ -41,15 +48,14 @@ const Sidebar = () => {
       <Heading as="h3" size="lg" mb={4} textAlign="center">
         Retax
       </Heading>
-    
-        <Input
-          type="text"
-          placeholder="Search"
-          value={searchText}
-          onChange={handleSearchChange}
-          mb={4}
-        />
-     
+
+      <Input
+        type="text"
+        placeholder="Search"
+        value={searchText}
+        onChange={handleSearchChange}
+        mb={4}
+      />
 
       <VStack spacing={4} align="start">
         {filteredLinks.map((link) => (
@@ -63,36 +69,35 @@ const Sidebar = () => {
       </VStack>
 
       <Box mt={16}>
-         <Divider />
+        <Divider />
       </Box>
 
       <VStack spacing={4} align="start" mt={4}>
-         <Link to="/settings">
-           <HStack spacing={2} _hover={{ textDecoration: "underline" }}>
-             <FaCog size={20} />
-             <Box>Settings</Box>
-           </HStack>
-         </Link>
-         <Link to="/help">
+        <Link to="/settings">
           <HStack spacing={2} _hover={{ textDecoration: "underline" }}>
-             <FaQuestionCircle size={20} />
-             <Box>Help</Box>
-           </HStack>
-         </Link>
-       </VStack>
+            <FaCog size={20} />
+            <Box>Settings</Box>
+          </HStack>
+        </Link>
+        <Link to="/help">
+          <HStack spacing={2} _hover={{ textDecoration: "underline" }}>
+            <FaQuestionCircle size={20} />
+            <Box>Help</Box>
+          </HStack>
+        </Link>
+      </VStack>
 
-       <Box mt={12}>
-         <Divider />
-       </Box>
+      <Box mt={12}>
+        <Divider />
+      </Box>
 
-       <Link to="/logout">
-         <HStack spacing={2} mt={6} _hover={{ textDecoration: "underline" }}>
-           <FaSignOutAlt size={20} />
-           <Box>Logout</Box>
-         </HStack>
-     </Link>
-     </Box>
-    
+      <Link to="/logout">
+        <HStack spacing={2} mt={6} _hover={{ textDecoration: "underline" }}>
+          <FaSignOutAlt size={20} />
+          <Box>Logout</Box>
+        </HStack>
+      </Link>
+    </Box>
   );
 };
 
