9f8f524aa5646c242736c3917ea104d2f9f18651
samarjeetpatankar <samarjeetpatankar@gmail.com>
2023-08-12 17:06:54 +0530

    updating pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,35 +1,15 @@
 // App.js
-import React, { useState } from "react";
+import React from "react";
 import { Box } from "@chakra-ui/react";
 import "./App.css";
 import AllFiles from "./Components/AllFiles/AllFiles";
-import { Routes, Route, Navigate, useNavigate } from "react-router-dom"; // Import useNavigate
-import Login from "./Components/LoginSIgnup/Login";
-import { AllRoutes } from "./Components/AllRoutes/AllRoutes";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const navigate = useNavigate(); 
-
   return (
     <Box className="App">
       <AllFiles />
-      <Routes>
-        <Route
-          path="/login"
-          element={<Login setIsLoggedIn={setIsLoggedIn} navigate={navigate} />} // Pass the navigate function to the Login component
-        />
-        {isLoggedIn ? <Route path="/" element={<AllRoutes />} /> : null}{" "}
-        {/* Conditional rendering based on isLoggedIn */}
-        {!isLoggedIn && (
-          <Route path="/*" element={<Navigate to="/login" />} />
-        )}{" "}
-        {/* Redirect all other paths to login */}
-      </Routes>
     </Box>
   );
 }
 
 export default App;
-
-
--- frontend/src/Components/Topbar/Topbar.jsx
+++ frontend/src/Components/Topbar/Topbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import {
   Box,
   IconButton,
- 
   Modal,
   ModalOverlay,
   ModalContent,
